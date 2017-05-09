0ee5dc3afb2d7c5f6e4c4452d35477dc176502a9
Joice <joicepazsilva@gmail.com>
2017-05-09 17:08:55 -0300

    Início do primeiro problema

---
+++ 1/1.c
@@ -0,0 +1,29 @@
+/*Faça um programa em C que gerencie os dados de produtos de uma
+loja através do uso de listas simplesmente encadeadas. Os dados
+dos produtos são: código, nome, quantidade em estoque, preço de
+compra, preço de venda.
+O programa deve apresentar um menu com as seguintes opções:
+1- Inserir dados de novo produto
+2- Consultar dados de um produto
+3- Imprimir dados de todos os produtos
+4- Imprimir produto mais caro
+5- Sair
+Obs.:
+- O usuário pode cadastrar produtos enquanto desejar, isto é, o
+limite de cadastro é a memória do computador.
+- Em caso de haver mais de um produto com maior preço de venda,
+todos devem ser impressos*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+#include <string.h>
+#include <ctype.h>
+
+int main(){
+	setlocale(LC_ALL,"portuguese");
+	
+	
+	printf("lalaal");
+	system("cls");
+	return 0;
+}
