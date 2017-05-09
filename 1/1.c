bb0052966e2b887aeb9491ddeb089f34b3a1e765
Joice <joicepazsilva@gmail.com>
2017-05-09 17:43:25 -0300

    Inserção adicionada.

--- 1/1.c
+++ 1/1.c
@@ -2,12 +2,14 @@
 loja através do uso de listas simplesmente encadeadas. Os dados
 dos produtos são: código, nome, quantidade em estoque, preço de
 compra, preço de venda.
+
 O programa deve apresentar um menu com as seguintes opções:
 1- Inserir dados de novo produto
 2- Consultar dados de um produto
 3- Imprimir dados de todos os produtos
 4- Imprimir produto mais caro
 5- Sair
+
 Obs.:
 - O usuário pode cadastrar produtos enquanto desejar, isto é, o
 limite de cadastro é a memória do computador.
@@ -19,11 +21,101 @@ todos devem ser impressos*/
 #include <string.h>
 #include <ctype.h>
 
+typedef struct no{
+	int cod, qtd;
+	char nome[100];
+	float pc, pv;
+	struct no *prox;
+}no;
+
+no *inicio;
+
+void criaLista(){
+	inicio=NULL;
+}
+
+no *criaNo(char nome[], int cod, int qtd, float pc, float pv){
+	no *aux;
+	aux = (no*) malloc(sizeof(no));
+	
+	if(aux){
+		aux->cod=cod;
+		aux->qtd=qtd;
+		aux->pc=pc;
+		aux->pv=pv;
+		strcpy(aux->nome, nome);
+		
+		aux->prox=NULL;
+	}
+	
+	return aux;
+}
+
+void insere(char nome[], int cod, int qtd, float pc, float pv){
+	no *aux;
+	aux = criaNo(nome, cod, qtd, pc, pv);
+	
+	aux->prox=inicio;
+	inicio=aux;
+}
+
+void percorre(){
+	no *aux;
+	aux=inicio;
+	
+	while(aux!=NULL){
+		printf("%s, ", aux->nome);
+		printf("código: %d, ", aux->cod);
+		printf("quantidade em estoque %d, ", aux->qtd);
+		printf("preço de compra %.2f, ", aux->pc);
+		printf("preço de venda %.2f.\n\n", aux->pv);
+		
+		aux=aux->prox;
+	}
+}
 int main(){
 	setlocale(LC_ALL,"portuguese");
+	int op, cod, qtd;
+	char nome[100], resp;
+	float pc, pv;
 	
+	criaLista();
 	
-	printf("lalaal");
-	system("cls");
+	printf("1- Inserir dados de novo produto\n2- Consultar dados de um produto\n3- Imprimir dados de todos os produtos\n4- Imprimir produto mais caro\n5- Sair\n\n");
+	scanf("%d", &op);
+	fflush(stdin);
+	
+	switch(op){
+		case 1:
+			do{
+				system("cls");
+				fflush(stdin);
+				printf("Digite o nome: \n");
+				gets(nome);
+				printf("Digite o código: \n");
+				scanf("%d", &cod);
+				printf("Digite a quantidade em estoque: \n");
+				scanf("%d", &qtd);
+				fflush(stdin);
+				printf("Digite o preço de compra: \n");
+				scanf("%f", &pc);
+				printf("Digite o preço de venda: \n");
+				scanf("%f", &pv);
+				
+				insere(nome, cod, qtd, pc, pv);
+				
+				system("cls");
+				printf("Deseja continuar? Digite \"S\" para sim.\n");
+				resp=getch();
+				resp=toupper(resp);
+			}while(resp=='S');
+			
+			
+			break;
+		default:
+			printf("Erro\n");
+			break;
+	}
+	
 	return 0;
 }
