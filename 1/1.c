245f6cb04e09f5e50137177fbbd3baedde99841a
Rafael Trindade <rafaeltrindade649@gmail.com>
2017-05-12 20:20:20 -0300

    melhorando a compatibilidade com sistemas Unix

--- 1/1.c
+++ 1/1.c
@@ -3,6 +3,49 @@
 #include <locale.h>
 #include <string.h>
 #include <ctype.h>
+#include <unistd.h>
+char SISTEMA;
+char apresenta(){
+	setlocale(LC_ALL,"portuguese");
+	/*
+	.::::.  ::      .::::.  ||
+	::  ::  ::      ::  ::  ||
+	::  ::  ::      ::::::  ||
+	*::::*  ::::::  ::  ::  ::
+    */
+	printf("\t.::::.  ::      .::::.  ||\n");
+	usleep(200000);
+	printf("\t::  ::  ::      ::  ::  ||\n");
+	usleep(200000);
+	printf("\t::  ::  ::      ::::::  ||\n");
+	usleep(200000);
+	printf("\t*::::*  ::::::  ::  ::  ::\n\n");
+	printf("\tPOR GENTILEZA, INFORME QUAL SISTEMA OPERACIONAL VC UTILIZA:\nD=DOS\nU=UNIX\n\n");
+	printf("\tDUVIDAS? DIGITE (i) PARA MAIS INFORMAÇÕES\n");
+	pergunta:
+	fflush(stdin);
+	scanf("%c",&SISTEMA);
+	SISTEMA=toupper(SISTEMA);
+	switch(SISTEMA){
+		case 'D':
+		system("cls");
+		fflush(stdin);
+		break;
+		case 'U':
+		system("clear");
+		fflush(stdin);
+		break;
+		case 'I':
+		printf("\nDOS = Window\nUNIX = Mac e Linux(há exeções)");
+		getchar();
+		goto pergunta;
+		break;
+		default:
+		printf("\nENTRADA INVALIDA, FAVOR DIGITAR SOMENTE AS LETRAS INDICADAS\n");
+		goto pergunta;
+	}
+	return SISTEMA;
+}
 
 typedef struct maior{
 	int cod, qtd;
@@ -23,11 +66,13 @@ typedef struct no{
 no *inicio;
 
 void criaLista(){
+	setlocale(LC_ALL,"portuguese");
 	inicio=NULL;
 	inicioMaior==NULL;
 }
 
 no *criaNo(char nome[], int cod, int qtd, float pc, float pv){//cria nós da estrutura de prods
+	setlocale(LC_ALL,"portuguese");
 	no *aux;
 	aux = (no*) malloc(sizeof(no));
 	
@@ -45,6 +90,7 @@ no *criaNo(char nome[], int cod, int qtd, float pc, float pv){//cria nós da estr
 }
 
 maior *criaNoMaior(int cod, int qtd, float pc, float pv, char nome[]){//cria nós da estrutura de prods caros
+	setlocale(LC_ALL,"portuguese");
 	maior *aux;
 	aux = (maior*)malloc(sizeof(maior));
 	
@@ -62,18 +108,20 @@ maior *criaNoMaior(int cod, int qtd, float pc, float pv, char nome[]){//cria nós
 }
 
 void insereMaior(int cod, int qtd, float pc, float pv, char nome[]){// insere na struct de prods caros
+	setlocale(LC_ALL,"portuguese");
 	maior *aux;
 	aux = criaNoMaior(cod, qtd, pc, pv, nome);
 	
 	aux->prox=inicioMaior;
 	inicioMaior=aux;
 }
-	
+
 void percorre(int i){//reaproveitando código através do parâmetro que indicará a ocasião
+	setlocale(LC_ALL,"portuguese");
 	if(i==1){
 		no *aux;
 		aux=inicio;
-	
+
 		while(aux!=NULL){
 			printf("%s, ", aux->nome);
 			printf("código: %d, ", aux->cod);
@@ -86,7 +134,7 @@ void percorre(int i){//reaproveitando código através do parâmetro que indicará a
 	}else{
 		maior *aux;
 		aux = inicioMaior;
-	
+
 		while(aux!=NULL){
 			printf("%s, ", aux->nome);
 			printf("código: %d, ", aux->cod);
@@ -100,6 +148,7 @@ void percorre(int i){//reaproveitando código através do parâmetro que indicará a
 }
 
 no *busca(int cod){
+	setlocale(LC_ALL,"portuguese");
 	no *aux;
 	aux=inicio;
 	
@@ -115,6 +164,7 @@ no *busca(int cod){
 
 
 int verifica(char nome[], int cod, int qtd, float pc, float pv){//verifica se existe código igual já cadastrado
+	setlocale(LC_ALL,"portuguese");
 	no *aux;
 	aux = inicio;
 	
@@ -134,6 +184,7 @@ int verifica(char nome[], int cod, int qtd, float pc, float pv){//verifica se ex
 }
 
 void verificaPreco(char nome[], int cod, int qtd, float pc, float pv){
+	setlocale(LC_ALL,"portuguese");
 	no *aux;
 	aux = inicio;
 	maior *aux2;
@@ -141,47 +192,55 @@ void verificaPreco(char nome[], int cod, int qtd, float pc, float pv){
 	aux2=criaNoMaior(cod, qtd, pc, pv, nome);
 
  	if(inicioMaior==NULL){ //se a lista estiver vazia o primeiro será o mais caro
-		inicioMaior = aux2;
-	
-	}else if(aux2->pv > inicioMaior->pv){
-		inicioMaior = aux2;
-		inicioMaior->prox = NULL;
-		
-	}else if(aux2->pv == inicioMaior->pv){
-		aux2->prox=inicioMaior;
-		inicioMaior = aux2;
-			
-	}
-	
-}	
+ 		inicioMaior = aux2;
 
-void insere(char nome[], int cod, int qtd, float pc, float pv){
-	no *aux;
-	int x, z;
-	x=verifica(nome, cod, qtd, pc, pv);
+ 	}else if(aux2->pv > inicioMaior->pv){
+ 		inicioMaior = aux2;
+ 		inicioMaior->prox = NULL;
+
+ 	}else if(aux2->pv == inicioMaior->pv){
+ 		aux2->prox=inicioMaior;
+ 		inicioMaior = aux2;
+
+ 	}
+
+ }	
+
+ void insere(char nome[], int cod, int qtd, float pc, float pv){
+ 	setlocale(LC_ALL,"portuguese");
+ 	no *aux;
+ 	int x, z;
+ 	x=verifica(nome, cod, qtd, pc, pv);
 
 	if(x==1){//se nao tiver codigo igual insere
 		aux = criaNo(nome, cod, qtd, pc, pv);
 		aux->prox=inicio;
 		inicio=aux;
-	
+
 		verificaPreco(nome, cod, qtd, pc, pv);//Irá verificiar e adicionar no struct dos produtos caros
-		system("pause");
+		if (SISTEMA == 'D')
+			system("pause");
+		else
+			getchar();
 	}else{
 		printf("Código existente. Insira novamente com outro código diferente.\n\n");
-		system("pause");
+		if (SISTEMA == 'D')
+			system("pause");
+		else
+			getchar();
 	}
 	
 }
 
 void pegaDados(){
+	setlocale(LC_ALL,"portuguese");
 	int cod, qtd;
 	char nome[100], resp;
 	float pc, pv;
 	
 	fflush(stdin);
 	printf("Digite o nome: \n");
-	gets(nome);	
+	fgets(nome,100,stdin);	
 	printf("Digite o código: \n");
 	scanf("%d", &cod);
 	printf("Digite a quantidade em estoque: \n");
@@ -191,7 +250,7 @@ void pegaDados(){
 		while(qtd<0){
 			printf("Digite uma quantidade em estoque maior ou igual a zero.\n");
 			scanf("%d", &qtd);
-			}
+		}
 	}
 	
 	fflush(stdin);
@@ -202,9 +261,9 @@ void pegaDados(){
 		while(pc<=0){
 			printf("Digite um preço de compra maior que zero.\n");
 			scanf("%f", &pc);
-			}
 		}
-		
+	}
+
 	printf("Digite o preço de venda: \n");
 	scanf("%f", &pv);
 	
@@ -212,9 +271,9 @@ void pegaDados(){
 		while(pv<=0){
 			printf("Digite um preço de venda maior que zero.\n");
 			scanf("%f", &pv);
-			}
 		}
-		
+	}
+
 	insere(nome, cod, qtd, pc, pv);
 	
 }
@@ -225,12 +284,16 @@ int main(){
 	int op, cod, qtd, c;
 	char nome[100], resp;
 	float pc, pv;
-	
+
+	SISTEMA=apresenta();
 	criaLista(); //cria a lista dos caros e a dos produtos
 	
 	printf("Bem-vindo. Digite os dados do primeiro produto: \n\n");
 	pegaDados();
-	system("cls");
+	if (SISTEMA == 'D')
+		system("cls");
+	else
+		system("clear");
 	
 	do{
 		printf("1- Inserir dados de novo produto\n2- Consultar dados de um produto\n3- Imprimir dados de todos os produtos\n4- Imprimir produto(s) mais caro(s)\n5- Sair\n\n");
@@ -238,57 +301,75 @@ int main(){
 		
 		switch(op){
 			case 1:
-				pegaDados();					
+			pegaDados();					
+			if (SISTEMA == 'D')
 				system("cls");
-				break;
-				
+			else
+				system("clear");
+			break;
+
 			case 2:
+			if (SISTEMA == 'D')
 				system("cls");
-				printf("Digite o código do produto desejado: \n");
-				scanf("%d", &cod);
-				
-				no *aux;
-				aux=busca(cod);
-				
-				if(aux!=NULL){
-					printf("%s, ", aux->nome);
-					printf("código: %d, ", aux->cod);
-					printf("quantidade em estoque %d, ", aux->qtd);
-					printf("preço de compra %.2f, ", aux->pc);
-					printf("preço de venda %.2f.\n\n", aux->pv);
-				}else{
-					printf("Não encontrado!");
-				}
-				break;
-				
+			else
+				system("clear");
+			printf("Digite o código do produto desejado: \n");
+			scanf("%d", &cod);
+
+			no *aux;
+			aux=busca(cod);
+
+			if(aux!=NULL){
+				printf("%s, ", aux->nome);
+				printf("código: %d, ", aux->cod);
+				printf("quantidade em estoque %d, ", aux->qtd);
+				printf("preço de compra %.2f, ", aux->pc);
+				printf("preço de venda %.2f.\n\n", aux->pv);
+			}else{
+				printf("Não encontrado!");
+			}
+			break;
+
 			case 3:
+			if (SISTEMA == 'D')
 				system("cls");
-				percorre(1);
-				break;
-				
+			else
+				system("clear");
+			percorre(1);
+			break;
+
 			case 4:
+			if (SISTEMA == 'D')
 				system("cls");
-				percorre(2);
-				break;
-				
+			else
+				system("clear");
+			percorre(2);
+			break;
+
 			case 5:
+			if (SISTEMA == 'D')
 				system("cls");
-				printf("Saindo...");
-				break;
-				
+			else
+				system("clear");
+			printf("Saindo...");
+			break;
+
 			default:
+			if (SISTEMA == 'D')
 				system("cls");
-				printf("Opção inválida...\n");
-				break;
+			else
+				system("clear");
+			printf("Opção inválida...\n");
+			break;
 		}
-	
+
 	if(op==5){//para o do...while quando a opção é 5 para que não pergunte se deseja continuar
 		break;
 	}
 	
 	printf("Deseja continuar? Digite \"1\" para sim.\n");
 	scanf("%d", &c);
-	}while(c==1);
+}while(c==1);
 
 return 0;
 }
